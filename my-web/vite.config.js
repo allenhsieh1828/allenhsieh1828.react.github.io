c26e5e563841f98248b8b4aa18186a6b6df628e6
allenhsieh1828 <allenhsieh1828@gmail.com>
2026-01-12 09:27:33 +0000

    fix: 修正部署路徑指向 my-web

--- my-web/vite.config.js
+++ my-web/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 建議使用相對路徑，這樣不管放在哪裡都能正確抓到 js/css/圖片
 })
